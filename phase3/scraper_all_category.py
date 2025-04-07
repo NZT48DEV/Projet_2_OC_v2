f09b7c90e14e660b802c270e7c1d0d718757feda
NZT48DEV <nzt48.dev@gmail.com>
2025-04-07 10:44:38 +0200

    Démarrage de la feature phase3

---
+++ phase3/scraper_all_category.py
@@ -0,0 +1,16 @@
+# Récupération de toutes les catégories du site
+# Un nouveau mode d'enregistrement avec un fichier CSV par categorie
+
+URL = "https://books.toscrape.com/index.html"
+
+urls = []
+
+
+def fetch_all_category_urls():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
