ae0b4e15528135f713222776179099f01063fda3
NZT48DEV <nzt48.dev@gmail.com>
2025-04-07 13:41:12 +0200

    Optimisation de la performance avec le Multithreading

--- phase3/scraper_all_category.py
+++ phase3/scraper_all_category.py
@@ -3,7 +3,9 @@ import os
 import sys
 import re
 import csv
+import time
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 if __name__ == "__main__" and __package__ is None:
     # Ajoute le dossier parent (racine du projet) à sys.path
@@ -65,7 +67,33 @@ def save_all_categories_to_csv(all_books_data, category_name):
         print(f"[ERREUR] Echec de la sauvegarder pour la catégorie '{category_name}' : {e}")
 
 
+def scrape_books_parallel(book_urls):
+    results = []
+
+    def process_url(index, url):
+        try:
+            print(f"Livre {index + 1}/{len(book_urls)} : {url}")
+            soup = fetch_page(url)
+            return extract_book_data(soup, url)
+        except Exception as e:
+            print(f"[ERREUR] Livre non traité ({url}) : {e}")
+            return None
+
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        future_to_url = {
+        executor.submit(process_url, index, url): url
+        for index, url in enumerate(book_urls)
+        }
+        for future in as_completed(future_to_url):
+            result = future.result()
+            if result:
+                results.append(result)
+
+    return results
+
+
 def main():
+    start_time = time.time()
     with requests.Session() as session:
         print(f"\nDébut du scraping de toutes les catégories/livres du site.\n")
         category_urls, category_names = fetch_all_category_urls(URL, session)
@@ -80,20 +108,10 @@ Lien de la catégorie {category_name} : {category_url}
 ______________________________________________________
 
 """)
-
         book_urls = fetch_category_urls(category_url, session)
-
-        all_books_data = []
-        for book_index, book_url in enumerate(book_urls, start=1):
-            try:
-                print(f"Livre {book_index}/{len(book_urls)} : {book_url}")
-                soup = fetch_page(book_url)
-                book_data = extract_book_data(soup, book_url)
-                all_books_data.append(book_data)
-                save_all_categories_to_csv(all_books_data, category_name)
-            except Exception as e:
-                print(f"[ERREUR] Livre non traité ({book_url}) : {e}")
-        
+        all_books_data = scrape_books_parallel(book_urls)
+        duration = time.time() - start_time
+        print(f"Durée d'exécution : {duration:.2f} secondes")
 
 if __name__ == "__main__":
     main()
